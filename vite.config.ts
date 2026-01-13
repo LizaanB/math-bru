0b1b4cb7c3e20d85626256949e1043d9d4554db1
Lizaanb <lizaanbotha20@gmail.com>
2026-01-13 21:11:05 +0200

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/math-bru/',
   server: {
     host: '0.0.0.0', // Expose on all network interfaces
     port: 3000,
